d8629039c23274d14021a25d4542b126644036cc
wontae <83wontae@gmail.com>
2023-07-31 10:47:26 +0900

    sdfsd

---
+++ ShootingGameCode/Source/ShootingGameCode/Public/BurstWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "BurstWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTINGGAMECODE_API ABurstWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
